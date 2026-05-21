f5fbc69348a5af1e5f1f6773b50fde42a285a2b7
manvi bhatt 36473 <manvibhatt1137@gmail.com>
2026-05-21 10:37:24 +0200

    Add files via upload

---
+++ book factory.py
@@ -0,0 +1,7 @@
+class Book:
+    def __init__(self,title,author):
+        self.title = title
+        self.author = author
+b1 = Book("Harry Potter","J.K.Rowling")
+print(f"name of the most popular book : {b1.title}")
+print(f"author:{b1.author}")
